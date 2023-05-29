162eef980d7be4336c5727711f56287ec72bfc4b
jwGreeff <61791154+jwGreeff@users.noreply.github.com>
2023-05-29 14:59:31 +0200

    Add files via upload

---
+++ Section 4 – Modelling Time Series Data/4.1 Load_Time_Series_Data.sql
@@ -0,0 +1,5 @@
+﻿/* Loading Data */
+/* See instructions file if error reading file. */
+SET datestyle = dmy;
+COPY time_series.location_temp(event_time, location_id, temp_celcius)
+FROM 'D:\Dropbox\BSC_Hons\Year 1\SQL Course 3 - Advanced PostgreSQL for Data Science Time Series\Exercises\04\data\location_temp.txt' DELIMITER ',';
---
+++ Section 4 – Modelling Time Series Data/4.6 Load_Utilization_Data.sql
@@ -0,0 +1,4 @@
+﻿/* Loading Data */
+SET datestyle = mdy;
+COPY time_series.utilization(event_time, server_id, cpu_utilization, free_memory, session_cnt)
+FROM 'D:\Dropbox\BSC_Hons\Year 1\SQL Course 3 - Advanced PostgreSQL for Data Science Time Series\Exercises\04\data\utilization.txt' DELIMITER ','
